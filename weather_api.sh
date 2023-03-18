f2593522cbde48c3c25f5306f967d9c176c5095e
root <root@Pcnico>
2023-03-18 15:18:45 +0100

    ajout de detail pour la meteo

--- weather_api.sh
+++ weather_api.sh
@@ -2,4 +2,7 @@ read -p "Vous souhaitez la méteo de quelle ville ?" location;
 weather_api="http://api.weatherapi.com/v1/current.json?key=2474183e9ec44d90888100353231603&q=$location&aqi=yes";
 temp_c=$(curl --silent $weather_api|jq -r ".current.temp_c");
 ville=$(curl --silent $weather_api|jq -r ".location.name");
-echo "A $ville il fait $temp_c °C";
+condition=$(curl --silent $weather_api|jq -r ".current.condition.text");
+wind=$(curl --silent $weather_api|jq -r ".current.wind_kph");
+wind_dir=$(curl --silent $weather_api|jq -r ".current.wind_dir");
+echo "A $ville il fait $temp_c °C, les condition seront $condition, avec un vent $wind_dir de $wind Km/h";
