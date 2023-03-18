df97396ff03bedeee864c5f96fab5789d8cf6406
root <root@Pcnico>
2023-03-18 13:13:43 +0100

    changement

--- quoteday_api.sh
+++ quoteday_api.sh
@@ -1,3 +1,3 @@
-number=$RANDOM%40;
+number=$(($RANDOM%50));
 echo $number;
-curl "https://zenquotes.io/api/quotes"|jq '.[$number].q';
+curl "https://zenquotes.io/api/quotes"|jq --argjson num "$number" '.[$num].q';
--- weather_api.sh
+++ weather_api.sh
@@ -1,3 +1,3 @@
 read -p "Vous souhaitez la méteo de quelle ville ?" location;
-curl "http://api.weatherapi.com/v1/current.json?key=2474183e9ec44d90888100353231603&q=$location&aqi=yes"|jq '.current.temp_c, .location.contry, .location.name';
-
+weather_api="http://api.weatherapi.com/v1/current.json?key=2474183e9ec44d90888100353231603&q=$location&aqi=yes";
+curl $weather_api|jq '.current.temp_c, .location.contry, .location.name';
