d79eca8f09ddbb5dbf1681c63d4cfe9e45d1db56
root <root@Pcnico>
2023-03-19 10:25:41 +0100

    choix de l'addresse imparfait

--- address_api.sh
+++ address_api.sh
@@ -1,3 +1,12 @@
 read -p "Ou souhaitez vous aller ?" my_address;
 echo "Vous voulez aller a : $my_address";
-curl --silent --compressed "https://api-adresse.data.gouv.fr/search/?q="{${my_address// /+}}"&limit=1"|jq '.features[].properties.label';
+address_url="https://api-adresse.data.gouv.fr/search/?q="{${my_address// /+}};
+tab_address=($(curl --silent --compressed $address_url|jq ' .features[].properties.label'));
+#tab_address=($(curl --silent --compressed $address_url|jq ' .features[].properties.label' | jq -s '.'));
+echo ${tab_address[@]};
+
+curl --silent --compressed $address_url|jq ' .features[].properties.label';
+read -p "quelle address ?" my_select;
+echo "vous avez choisi :";
+echo ${tab_address[$my_select]};
+
